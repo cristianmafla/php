6121df3d1b2ead2cc58bc44980502f753167bb56
cristianmafla <cristiancaxi@gmail.com>
2018-10-07 13:05:26 -0500

    conexion PDO

--- PDO/models/mdl_query_build.php
+++ PDO/models/mdl_query_build.php
@@ -1,3 +1,26 @@
 <?php
 
-    echo 'hola conexion';
+class Conx {
+    //PARAMETROS DE CONEXION
+    static private $dsn = 'mysql:host=localhost;dbname=colombia';
+    static private $user = 'root';
+    static private $pass = '';
+    
+    public function __construct() {
+    }
+    
+    public function cnx(){
+        try {
+            //CAPTURADOR DE ERRORES Y CAPTURADOR DE CARACTERES utf8
+            $options = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
+            );
+            $dbh = new PDO(self::$dsn, self::$user, self::$pass,$options);
+            return $dbh;
+        }catch(PDOException $e) {
+            $msn = 'connection error: '.$e->getMessage();
+            echo $msn;
+        }
+    }
+}
